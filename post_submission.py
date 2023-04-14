11c196952dc7846455a51ba2d8358961eeac7c4e
Jacopo Margutti <jacopo.margutti@gmail.com>
2023-04-14 13:52:01 +0200

    Add files via upload

---
+++ post_submission.py
@@ -0,0 +1,27 @@
+import requests
+import uuid
+
+headers = {
+    'Authorization': f'Token <my-token>',
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
+
+submission = {
+    'meta': {
+        'instanceID': f'uuid:{uuid.uuid4()}'
+    },
+    'question1': 'answer1',
+    'group1': {
+        'question2': 'answer2'
+    }
+}
+
+data_request = requests.post(
+    f'https://kcnew.ifrc.org/api/v1/submissions',
+    json={
+        "id": "<my-asset>",
+        "submission": submission
+    },
+    headers=headers
+)
